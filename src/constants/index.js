4064a0d84dacc00512197389e679421acbe31ea5
Ratnesh Pawar <ratneshpawar00@gmail.com>
2023-10-16 19:37:37 -0700

    a

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,7 +23,6 @@ import {
     jobit,
     tripguide,
     threejs,
-  
 
   } from "../assets";
   
@@ -52,7 +51,7 @@ import {
       icon: mobile,
     },
     {
-      title: "Front-End Developer",
+      title: "Machine Learning Engineer",
       icon: backend,
     },
     {
@@ -111,7 +110,7 @@ import {
       icon: figma,
     },
     {
-      name: "docker",
+      name: "python",
       icon: docker,
     },
   ];
@@ -120,27 +119,28 @@ import {
     {
       title: "Sofware Developer Intern",
       company_name: "iBase Electrosoft",
-      //icon: starbucks,
+      icon: tesla,
       iconBg: "#383E56",
       date: "Jan 2022 - July 2022",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Developing and maintaining projects using JavaScript/Css and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Migrated legacy applications from AngularJS to React and used AWS EC2 instances for deployment.",
         "Participating in code reviews and providing constructive feedback to other developers.",
       ],
     },
     {
       title: "Student Assistant",
       company_name: "California State University, Dominguez Hills",
-      //icon: tesla,
+      icon: starbucks,
       iconBg: "#383E56",
       date: "August 2023 - Present",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        " Achieved a 95% resolution rate for IT-related issues, ensuring professors and students experienced minimal disruptions in classroom activities.",
+        "Improved website user engagement by 30% through regular updates, resulting in increased access to course materials and resources.",
+        " Contributed to a 25% increase in event attendance through effective digital content creation and online registration management, enhancing departmental event outcomes.",
+        "Automated 50% of the Departmental tasks like Workshop Registration, Student Checklist, Class Schedule, etc.",
       ],
     },
 
@@ -149,28 +149,28 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
+        "I had the pleasure of having Ratnesh as a Software Developer Intern at iBase. His frontend development skills improved user engagement, and his role in migrating legacy applications to React and deploying on AWS was impressive. Ratnesh is a talented and dedicated software developer.",
+      name: "Nakul Deshmukh",
+      designation: "CEO",
+      company: "iBase Electrosoft",
+      image: "https://randomuser.me/api/portraits/men/4.jpg",
     },
-    {
-      testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-      testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
+//    {
+//      testimonial:
+//        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+//      name: "Chris Brown",
+//      designation: "COO",
+ //     company: "DEF Corp",
+ //     image: "https://randomuser.me/api/portraits/men/5.jpg",
+//    },
+//    {
+//      testimonial:
+ //       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+//      name: "Lisa Wang",
+ //     designation: "CTO",
+ //     company: "456 Enterprises",
+ //     image: "https://randomuser.me/api/portraits/women/6.jpg",
+ //   },
   ];
   
   const projects = [
@@ -200,46 +200,46 @@ import {
       source_code_link: "https://github.com/ratneshpawar1/colosseum",
     },
     {
-      name: "Job IT",
+      name: "CyberBullying Detection In Social Media.",
       description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+        "A machine Learning Model Which can detect Cyber-Bullying Content with accuracy of 90%. It allows to divide the dataset in percentage and shows the result in graphical format.",
       tags: [
         {
-          name: "react",
+          name: "python",
           color: "blue-text-gradient",
         },
         {
-          name: "restapi",
+          name: "nlp",
           color: "green-text-gradient",
         },
         {
-          name: "scss",
+          name: "machinlearning",
           color: "pink-text-gradient",
         },
       ],
       image: jobit,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/ratneshpawar1/Cyber-bullying-detection-",
     },
     {
-      name: "Trip Guide",
+      name: "Gesture Controlled Car Game",
       description:
-        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+        "Successfully created an interactive gesture-based video game using Python, comprising three main phases: developed a hand pose recognition program, designed an engaging car game, and seamlessly integrated both programs to deliver an immersive gaming experience. This project showcased a practical application of Python programming skills and a passion for innovative game development.",
       tags: [
         {
-          name: "nextjs",
+          name: "Python",
           color: "blue-text-gradient",
         },
         {
-          name: "supabase",
+          name: "numpy",
           color: "green-text-gradient",
         },
         {
-          name: "css",
+          name: "opencv",
           color: "pink-text-gradient",
         },
-      ],
+      ], 
       image: tripguide,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/ratneshpawar1/Gesture-conrtolled-car-game",
     },
   ];
   
